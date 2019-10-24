f496c58de4b8b5f5a501b72e31d6771661184c9b
VEENIT SINGH <123sveenit@gmail.com>
2019-10-24 21:55:31 +0530

    keepcoding

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -1,16 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void printHanoiSteps(int n, char src, char dest, char helper) {
-    if (n == 0) return;
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define pb push_back
 
-    printHanoiSteps(n-1, src, helper, dest);
-    cout << n << ": " << src << "-->" << dest << endl;
-    printHanoiSteps(n-1, helper, dest, src);
+void tower_of_hanoi(int n,string source,string destination,string helper){
+	//no disks left
+	if (n==0){
+		return ;
+	}
+	//move n-1 disks from src to helper
+	tower_of_hanoi(n-1,source,helper,destination);
+	cout<<"Move "<<n<<"th disk from "<<source<<" to "<<destination<<endl;
+	tower_of_hanoi(n-1,helper,destination,source);
+	return ;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    printHanoiSteps(n, 'A', 'B' , 'C');
+int32_t main(){
+	
+	ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//  code starts
+	int n;cin>>n;
+	string helper,source,destination;
+	cin>>source>>destination>>helper;
+	tower_of_hanoi(n,source,destination,helper);
+	
+	return 0;
 }
